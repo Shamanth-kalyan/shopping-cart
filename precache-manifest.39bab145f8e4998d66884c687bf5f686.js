5ba2f23cdc18197fec2c3f513889108b2bc8b960
Shamanth-kalyan <shamanth.kalyan@gmail.com>
2020-04-20 08:58:27 +0530

    Updates

---
+++ precache-manifest.39bab145f8e4998d66884c687bf5f686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e3a8765f69e8d8b81f74e0aabcbca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af78ba55e701672220d3",
+    "url": "/static/css/main.0f08f0fa.chunk.css"
+  },
+  {
+    "revision": "010b2807fc1dd28cd9c8",
+    "url": "/static/js/2.1e1bd3b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1e1bd3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af78ba55e701672220d3",
+    "url": "/static/js/main.afa90e75.chunk.js"
+  },
+  {
+    "revision": "305ebb44a25b9279e4ad",
+    "url": "/static/js/runtime-main.8fdaee4a.js"
+  }
+]);
